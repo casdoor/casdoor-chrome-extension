cf3826e62b1de6b878d65a056af6fecb0f151638
wenxuan70 <55948263+wenxuan70@users.noreply.github.com>
2022-08-09 23:41:05 +0800

    feat: login this extension through casdoor (#1)
    * feat: login this extension through casdoor
    
    * docs: modify document image
    
    * fix: replcace window.onload method
    
    Co-authored-by: wenxuan70 <t736660416@gamil.com>
    Co-authored-by: wenxuan70 <t736660416@gmail.com>

---
+++ src/oauth.js
@@ -0,0 +1,41 @@
+"use strict";
+
+const config = {
+    applicationName: "your application name", // e.g. "application_123456"
+    endpoint: "your backend url", // e.g. "http://localhost:8000"
+    clientId: "your client id", // e.g. "145c8aaed59b9338672d"
+    chromeExtensionId: "your chrome extension id" // e.g. "abcdefgffeplfamihkpcabdhllpilbdd"
+};
+
+const sdk = new Sdk(config);
+
+function login() {
+    sdk.login(accessToken => {
+        if (accessToken) {
+            alert("Login successful!");
+            chrome.storage.sync.set({accessToken: accessToken}, () => {
+                sdk.getUserProfile(accessToken)
+                    .then(userProfile => displayUserProfile(userProfile));
+            });
+        } else {
+            alert("Login failed!");
+        }
+    });
+}
+
+function logout() {
+    chrome.storage.sync.set({accessToken: ""}, () => clearUserProfile());
+}
+
+function displayUserProfile(userProfile) {
+    document.getElementById("user").innerHTML = `
+        <img alt="${userProfile.name}" src="${userProfile.picture}" width="50px" height="25px"/>
+        <p>Hi, ${userProfile.name}</p>
+    `;
+    document.getElementById("loginOrLogout").innerText = "Logout";
+}
+
+function clearUserProfile() {
+    document.getElementById("user").innerHTML = "";
+    document.getElementById("loginOrLogout").innerText = "Login";
+}
